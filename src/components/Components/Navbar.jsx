01fe5148a1ef1206d9af741a48fe9bbb2980bdf2
Orlando <orlando.arias.j@gmail.com>
2025-04-01 21:20:50 -0300

    fix navbar responsive

--- src/components/Components/Navbar.jsx
+++ src/components/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import DefaultDiv from "./defaultDiv";
 export default function App() {
   return (
     <DefaultDiv className={"relative pt-4"}>
-      <div className="w-fit mx-auto py-2 px-6 rounded-4xl flex items-center gap-6 text-sm text-gray-200 font-semibold">
+      <div className="w-fit mx-auto py-2 sm:px-6 rounded-4xl flex items-center  sm:gap-6 text-sm text-gray-200 font-semibold flex-wrap">
         <a class="p-1 px-4 group relative overflow-hidden" href="#">
           <span class="">Sobre Mi</span>
           <div class="absolute -left-[100%] bottom-0 bg-cyan-500 h-[2px] w-full transition-all duration-500 group-hover:-left-[0%]"></div>
