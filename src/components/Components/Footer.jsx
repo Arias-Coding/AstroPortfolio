8739f414073dae85427e0ed4d03a68b85f44d333
Orlando <orlando.arias.j@gmail.com>
2025-04-01 21:24:09 -0300

    update

--- src/components/Components/Footer.jsx
+++ src/components/Components/Footer.jsx
@@ -2,7 +2,7 @@ import DefaultDiv from "./defaultDiv";
 
 export default function App() {
   return (
-    <DefaultDiv bgColor={""} className={"pt-22 lg:pt-60 pb-26"}>
+    <DefaultDiv bgColor={""} className={"pt-35 lg:pt-60 pb-26"}>
       <h3 className="text-2xl lg:text-3xl font-bold">Contact Me</h3>
       <p className="text-md text-gray-400 2xl:w-145 mt-7">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore nisi
